dc97afac8ac69fe214704586a608354fd961d636
HungPhan <hungpv@atmarkcafe.org>
2018-07-04 15:49:09 +0700

    refactor

--- lib/kansuji.rb
+++ lib/kansuji.rb
@@ -1,14 +1,14 @@
 module Kansuji
   class << self; attr_reader :init_num_char end
   @init_num_char = { 10**68 => '無量大数', 10**64 => '不可思議', 10**60 => '那由他', 10**56 => '阿僧祇',
-                     10**52 => '恒河沙', 10**48 => '極', 10**44 => '載', 10**40 => '正', 10**36 => '澗',
-                     10**32 => '溝', 10**28 => '穣', 10**24 => '𥝱', 10**20 => '垓', 10**16 => '京', 10**12 => '兆',
-                     10**8 => '億', 10000 => '万', 1000 => '千', 100 => '百', 10 => '十', 9 => '九', 8 => '八',
-                     7 => '七', 6 => '六', 5 => '五', 4 => '四', 3 => '三', 2 => '二', 1 => '一', 0 => '零' }
+                     10**52 => '恒河沙', 10**48 => '極', 10**44 => '載', 10**40 => '正', 
+                     10**36 => '澗', 10**32 => '溝', 10**28 => '穣', 10**24 => '𥝱', 
+                     10**20 => '垓', 10**16 => '京', 10**12 => '兆', 10**8 => '億', 10000 => '万',
+                     1000 => '千', 100 => '百', 10 => '十', 9 => '九', 8 => '八', 7 => '七',
+                     6 => '六', 5 => '五', 4 => '四', 3 => '三', 2 => '二', 1 => '一', 0 => '零' }
 end
 
 class Integer
-  include Kansuji
   def num_to_kan(num)
     return Kansuji.init_num_char[num] unless Kansuji.init_num_char[num].nil?
     key, value = Kansuji.init_num_char.find { |k, v| num / k >= 1 }
@@ -26,7 +26,6 @@ class Integer
 end
 
 class String
-  include Kansuji
   def to_number(kan = self.gsub(/[\w]|[\s]/, ''))
     return 0 if kan == ''
     return Kansuji.init_num_char.invert[kan] unless Kansuji.init_num_char.invert[kan].nil?
